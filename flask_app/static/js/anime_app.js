cc0ff606145b0167e87c7bdfa1bdcb4b0327e55e
Shawna Hightower <sdhightower6@gmail.com>
2022-02-27 14:59:55 -0700

    tbs

--- flask_app/static/js/anime_app.js
+++ flask_app/static/js/anime_app.js
@@ -3,9 +3,11 @@ animeForm.addEventListener('submit', function(e){
     e.preventDefault('')
 
 let anime = document.querySelector('#anime-name').value
-    fetch(`https://api.myanimelist.net/v2/anime/${anime}`)
+    fetch(`https://api.myanimelist.net/v2/anime?q=${anime}`)
     .then( response => response.json())
     .then( data => {
-        
+        var img = data['images']['jpg']['image_url']
+        var imgdiv = document.querySelector('#anime-img')
+        imgdiv.innerHTML = `<img src = "${img}" alt="">`
     })
 })
