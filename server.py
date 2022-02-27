d761f4813b882d3d3c09a2fd835a427aaf7f67ae
Shawna Hightower <sdhightower6@gmail.com>
2022-02-26 18:52:50 -0700

    updates

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
+from sqlite3 import connect
+from flask import render_template
 from flask_app import app
-from flask_app.controllers import controller_shows
+
+@app.route("/")
+def index():
+    return render_template("index.html", token="Please connect")
 
 if __name__=="__main__":
     app.run(debug=True)
