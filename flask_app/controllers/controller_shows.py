512c1e5bd99fe49c5aa6bf373c7366de138bc5b5
Shawna Hightower <sdhightower6@gmail.com>
2022-02-26 20:38:21 -0700

    troubleshooting routing

--- flask_app/controllers/controller_shows.py
+++ flask_app/controllers/controller_shows.py
@@ -1,6 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect, request
-from flask_app.models.model_show import Show
+from flask import render_template
 
 #display route
 @app.route('/')
--- server.py
+++ server.py
@@ -1,10 +1,5 @@
-from sqlite3 import connect
-from flask import render_template
 from flask_app import app
-
-@app.route("/")
-def index():
-    return render_template("index.html", token="Please connect")
+from flask_app.controllers import controller_shows
 
 if __name__=="__main__":
     app.run(debug=True)
