d3d1c9b517067b966a585f988a7ad67df1c50320
Shawna Hightower <sdhightower6@gmail.com>
2022-03-05 20:44:42 -0700

    🤯🤯🤯🤯🤯

--- flask_app/controllers/controller_shows.py
+++ flask_app/controllers/controller_shows.py
@@ -1,7 +1,9 @@
 from flask_app import app
 from flask import render_template
 
+from flask_app.models.model_show import Show
+
 #display route
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
