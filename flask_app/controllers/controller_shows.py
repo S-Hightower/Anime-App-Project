d0b6e075cb8227755ca39254d486cac5fcc06a52
Shawna Hightower <sdhightower6@gmail.com>
2022-03-07 09:52:20 -0700

    updates

--- flask_app/controllers/controller_shows.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-from flask import render_template
-
-from flask_app.models.model_show import Show
-
-#display route
-@app.route('/', methods=['GET'])
-def index():
-    return render_template("index.html")
--- server.py
+++ server.py
@@ -1,5 +1,30 @@
-from flask_app import app
-from flask_app.controllers import controller_shows
+from unicodedata import name
+from flask import Flask, render_template, request
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == "POST":
+        q = request.form['q']
+
+        jikan_url = requests.get(
+            f'https://api.jikan.moe/v4/anime?q={q}')
+
+        responseData = jikan_url.json()
+
+        image_url = (responseData['images']['jpg']['image_url'])
+        title = (responseData['title'])
+        title_english = (responseData['title']['title_english'])
+        title_japanese = (responseData['title']['title_japanese'])
+        type = (responseData['type'])
+        airing = (responseData['airing'])
+        synopsis = (responseData['synopsis'])
+
+    return render_template('index.html', image_url=image_url, title=title, title_english=title_english, title_japanese=title_japanese, type=type, airing=airing, synopsis=synopsis)
 
 if __name__=="__main__":
     app.run(debug=True)
