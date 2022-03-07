8508613892fd6ae66f3a69df3f608b33a4473c5a
Shawna Hightower <sdhightower6@gmail.com>
2022-03-07 16:21:04 -0700

    successfully pulling data

--- server.py
+++ server.py
@@ -16,12 +16,26 @@ def index():
             f'https://api.jikan.moe/v4/anime?q={input}')
 
         responseData = jikan_url.json()
+        # response comes back as dictionary
+        # pprint(responseData.items())
+        # print('*********testing results**********')
+        # print('data' in responseData)
+        # pprint(responseData['data'])
+        # print('title' in responseData['data'])
+        # print(*responseData['data'], sep = ", ")
 
-        pprint(responseData)
+        responseList = [responseData['data']]
+        # response successfully saved as list
+        print('*********working response list**********')
+        # pprint(responseList[0][0]['images']['jpg']['image_url'])
+        # pprint(responseList[0][0]['title'])
+        # pprint(responseList[0][0]['title_english'])
+        # pprint(responseList[0][0]['title_japanese'])
+        # pprint(responseList[0][0]['type'])
+        # pprint(responseList[0][0]['airing'])
+        # pprint(responseList[0][0]['synopsis'])
 
-        
-
-    return render_template('index.html', responseData=responseData)
+    return render_template('index.html', responseData=responseData, responseList=responseList)
 
 if __name__=="__main__":
     app.run(debug=True)
